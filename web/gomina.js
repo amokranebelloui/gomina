dac6a4c7bd5ef506a05bef8fc5e9b23149b6bfe3
Amokrane Belloui <amokrane.belloui@gmail.com>
2017-09-16 19:48:36 +0200

    Architecture diagram d3js, convert to React component

--- web/gomina.js
+++ web/gomina.js
@@ -45,6 +45,36 @@ function Menu(props) {
     );
 }
 
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        //this.state = {isToggleOn: props.toggled};
+
+        // This binding is necessary to make `this` work in the callback
+        //this.toggle = this.toggle.bind(this);
+    }
+
+    toggle(e) {
+        /*
+         this.setState(prevState => ({
+         isToggleOn: !prevState.isToggleOn
+         }));
+         */
+        //this.setState({isToggleOn: e})
+        if (this.props.onToggleChanged) {
+            this.props.onToggleChanged(e)
+        }
+    }
+
+    render() {
+        return (
+            <button onClick={e => this.toggle(!this.props.toggled)}>
+                {this.props.toggled ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+}
+
 class Badge extends React.Component {
     constructor(props) {
         super(props);
