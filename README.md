0fcd3a1b27e8daf93eca1e670ee10dc971ce5dfe
Amokrane Belloui <amokrane.belloui@gmail.com>
2017-09-17 08:59:26 +0200

    Updating state using sockJS POC

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Lib Location
 <script src="https://unpkg.com/react@latest/dist/react.js"></script>
 <script src="https://unpkg.com/react-dom@latest/dist/react-dom.js"></script>
 <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+<script src="http://cdn.sockjs.org/sockjs-0.3.4.min.js"></script>
 
     
--- src/main/java/org/neo/gomina/WebVerticle.java
+++ src/main/java/org/neo/gomina/WebVerticle.java
@@ -1,11 +1,18 @@
 package org.neo.gomina;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+import io.vertx.ext.web.handler.sockjs.SockJSHandlerOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSSocket;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebVerticle extends AbstractVerticle {
 
@@ -16,7 +23,42 @@ public class WebVerticle extends AbstractVerticle {
         logger.info("Starting...");
         Router router = Router.router(vertx);
 
+        SockJSHandlerOptions options = new SockJSHandlerOptions().setHeartbeatInterval(2000);
+        SockJSHandler sockJSHandler = SockJSHandler.create(vertx, options);
+        List<SockJSSocket> sockets = new ArrayList<>();
+        sockJSHandler.socketHandler(sockJSSocket -> {
+            logger.info("Handling SockJS " + sockJSSocket);
+            logger.info("Started " + sockJSSocket.writeHandlerID());
+            sockets.add(sockJSSocket);
+            // Just echo the data back
+            sockJSSocket.handler(buffer -> {
+                logger.info("Event " + buffer.toString());
+                sockJSSocket.write(buffer);
+            });
+            sockJSSocket.endHandler(Void -> {
+                //logger.info("Event " + buffer.toString());
+                logger.info("Ended " + sockJSSocket.writeHandlerID());
+                sockets.remove(sockJSSocket);
+            });
+        });
+        router.route("/realtime/*").handler(sockJSHandler);
+
         router.route("/*").handler(StaticHandler.create("web").setCachingEnabled(false).setIndexPage("index.html"));
+
+        Thread thread = new Thread(() -> {
+            while (true) {
+                String message = "Sample " + System.currentTimeMillis();
+                Buffer buffer = Buffer.buffer(message);
+                sockets.forEach(socket -> socket.write(buffer));
+                try {
+                    Thread.sleep(1000);
+                }
+                catch (InterruptedException e) {
+                    logger.info("", e);
+                }
+            }
+        });
+        thread.start();
 
         HttpServer server = vertx.createHttpServer();
         server.requestHandler(router::accept).listen(8080);
