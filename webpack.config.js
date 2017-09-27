022f0fe81441fb93d1237e384401a41227409f9e
Amokrane Belloui <amokrane.belloui@gmail.com>
2017-09-28 00:38:58 +0200

    rambling with optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,13 @@ var path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+function isVendor({ resource }) {
+    return resource &&
+        resource.indexOf('node_modules') >= 0 &&
+        resource.match(/\.js$/);
+}
 
 module.exports = {
     /*
@@ -17,6 +24,7 @@ module.exports = {
     entry: {
         components: './web/components/components.js',
         app: './web/components/application.js'
+        //react: ['react', 'react-router', 'react-dom']
     },
     plugins: [
         new CleanWebpackPlugin(['web/dist']),
@@ -26,7 +34,51 @@ module.exports = {
             inject: 'body',
             filename: 'index.html'
         }),
-        new webpack.optimize.CommonsChunkPlugin({name: 'shared'})
+        new webpack.optimize.CommonsChunkPlugin({name: 'shared'}),
+        /*
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: module => isVendor(module)
+        }),
+        new webpack.optimize.CommonsChunkPlugin({name: 'shared', chunks: ['app', 'components']}),
+        */
+        /*
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'node-static',
+            //filename: 'node-static.js',
+            minChunks(module, count) {
+                var context = module.context;
+                return context && context.indexOf('node_modules') >= 0;
+            },
+        }),
+        */
+        /*
+        new webpack.optimize.CommonsChunkPlugin({name: 'react', chunks: ['react']}),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            chunks: ['app', 'components', 'react'], minChunks: isVendor
+        }),
+        new webpack.optimize.CommonsChunkPlugin({name: 'shared', chunks: ['app', 'components']}),
+        */
+        ///new webpack.optimize.CommonsChunkPlugin({name: 'shared', chunks: ['components', 'app']}),
+        ///new webpack.optimize.CommonsChunkPlugin({name: 'vendor2', chunks: ['react', 'shared'] }),
+        //new webpack.optimize.CommonsChunkPlugin({name: 'vendor', chunks: ['components', 'app', 'vendor2'], minChunks: isVendor}),
+
+        //new webpack.optimize.CommonsChunkPlugin({name: 'vendor', chunks: ['components', 'app']}),
+        //new webpack.optimize.CommonsChunkPlugin({name: 'shared'}),
+
+        // PROD Plugins
+        /*
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false }
+        }),
+        new BundleAnalyzerPlugin({analyzerMode: 'static'})
+        */
     ],
     output: {
         path: path.resolve(__dirname, 'web/dist'),
