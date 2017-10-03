323116722069a8bbc3dbc1e440db9680a3b6f991
Amokrane Belloui <amokrane.belloui@gmail.com>
2017-10-03 23:24:08 +0200

    Interactive d2 archi diagram

--- web/components/archi-diagram.js
+++ web/components/archi-diagram.js
@@ -13,49 +13,62 @@ class Diagram extends React.Component {
         this.state = {active: true}
     }
     componentDidMount() {
-        console.info("mounted")
         this.createDiagram()
     }
     componentDidUpdate() {
-        console.info("updated")
-        this.createDiagram()
+        // update ?
+        //console.info("diagram prop updated")
     }
     componentWillUnmount() {
-        console.info("destroy")
         var el = this.node;
         console.info(el);
         //d3.destroy(el); // Doesn't work
         //d3.select("svg").remove(); // Is it necessary?
     }
     createDiagram() {
-        //const svg = this.node;
+        //const svg1 = this.node;
         const positions = this.props.components;
         const dependencies = this.props.dependencies;
-        var svg = d3.select("svg");
+        var svg = d3.select(this.node);
 
-        console.info(svg);
+        //console.info("$$", svg, d3.select(svg1));
         var comps = svg.selectAll("g node-group").data(positions)
 
         svg.selectAll("line").data(dependencies).enter()
             .append("line")
-            .attr("x1", function (d) { console.log(positions, d); return positions[d.from].x; })
-            .attr("x2", function (d) { console.log(positions, d); return positions[d.to].x; })
-            .attr("y1", function (d) { console.log(positions, d); return positions[d.from].y; })
-            .attr("y2", function (d) { console.log(positions, d); return positions[d.to].y; })
+            .attr("x1", function (d) { return positions[d.from].x; })
+            .attr("x2", function (d) { return positions[d.to].x; })
+            .attr("y1", function (d) { return positions[d.from].y; })
+            .attr("y2", function (d) { return positions[d.to].y; })
             .attr("stroke-width", 1)
             .attr("stroke", "black");
+
+        const redrawlines = () => {
+            svg.selectAll("line").data(dependencies)
+                .attr("x1", function (d) { return positions[d.from].x; })
+                .attr("x2", function (d) { return positions[d.to].x; })
+                .attr("y1", function (d) { return positions[d.from].y; })
+                .attr("y2", function (d) { return positions[d.to].y; })
+                .attr("stroke-width", 1)
+                .attr("stroke", "black");
+
+        };
 
         var div = d3.select("body").append("div")
             .attr("class", "tooltip")
             .style("opacity", 0);
 
-
         var comp = comps.enter()
             .append("g")
+            .attr("id", function(d) { return "comp-" + d.name })
             .attr("class", "node-group")
             .attr("transform", function(d) {
                 return "translate(" + d.x + "," + d.y + ")"
             })
+            .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended))
             .on("mouseenter", function(d) {
                 div.transition().duration(200).style("opacity", .9);
                 div.html(d.name + "<br/><hr> with some short description text of "  + d.name + " service")
@@ -73,7 +86,7 @@ class Diagram extends React.Component {
         comp.append("circle")
             .attr("r", function(d) { return 30/*d.r*/; })
             .attr("stroke", function(d) { return "yellow"/*d.color*/; })
-            .attr("fill", "lightblue")
+            .attr("fill", "lightblue");
 
         comp.append("text")
             .text(function(d) { return d.name })
@@ -81,13 +94,37 @@ class Diagram extends React.Component {
             .attr("dy", ".35em")
             .attr("text-anchor", "middle")
 
+        function dragstarted(d) {
+            //console.info("drag start", d, this);
+            d.dragging = true;
+            d3.select(this).select("circle").attr("fill", "#8cccef");
+            d3.select(this).interrupt().raise().classed("active", true);
+            d3.event.sourceEvent.stopPropagation();
+        }
+
+        function dragged(d) {
+            //console.info("drag", d3.event.x, d3.event.y, d, this, d3.select(this));
+            d.x = d3.event.x;
+            d.y = d3.event.y;
+            redrawlines();
+            d3.select(this).attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")"
+            });
+        }
+
+        function dragended(d) {
+            //console.info("drag end", d, this);
+            d.dragging = false;
+            d3.select(this).classed("active", false);
+            d3.select(this).select("circle").attr("fill", "lightblue");
+        }
     }
     render() {
         return (
             <div>
                 <Toggle toggled={this.state.active} onToggleChanged={value => {this.setState({active: value})}} />
                 <br/>
-                {this.state.active && <svg ref={node => this.node = node} width="960" height="450"></svg>}
+                <svg ref={node => this.node = node} width="960" height="450"></svg>
             </div>
         );
     }
