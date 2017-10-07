f7f6a87b619cfb0d72754b6ea3464467edbac1e8
Amokrane Belloui <amokrane.belloui@gmail.com>
2017-10-07 22:53:45 +0200

    Passwords

--- src/test/java/org/neo/gomina/model/security/PasswordsTest.java
+++ src/test/java/org/neo/gomina/model/security/PasswordsTest.java
@@ -3,9 +3,19 @@ package org.neo.gomina.model.security;
 import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
 
+import java.util.Scanner;
+
 import static org.fest.assertions.Assertions.assertThat;
 
 public class PasswordsTest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Password:");
+        String password = scanner.next();
+        System.out.println(new String(Base64.encodeBase64(password.getBytes())));
+    }
+
     @Test
     public void getRealPassword() throws Exception {
         Passwords passwords = new Passwords();
