517324223b5735efdbb8be51c93f4d1b05a15362
Amokrane Belloui <amokrane.belloui@gmail.com>
2018-03-05 22:17:00 +0100

    SSH Caching

---
+++ src/main/java/org/neo/gomina/plugins/ssh/SshCache.kt
@@ -0,0 +1,52 @@
+package org.neo.gomina.plugins.ssh
+
+import com.thoughtworks.xstream.XStream
+import org.apache.logging.log4j.LogManager
+import org.neo.gomina.plugins.ssh.impl.SshDetails
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.util.*
+
+class SshCache {
+
+    companion object {
+        private val logger = LogManager.getLogger(SshCache::class.java)
+    }
+
+    private val xStream = XStream()
+    private val cache = HashMap<String, SshDetails>()
+
+    fun getDetail(host: String, folder: String): SshDetails? {
+        val key = host + "-" + folder.replace("/".toRegex(), "-").replace("\\\\".toRegex(), "-")
+        val cacheFile = File(".cache/ssh-$key")
+        return when {
+            cache.containsKey(key) -> cache[key]
+            cacheFile.exists() -> try {
+                    val sshDetails = xStream.fromXML(cacheFile) as SshDetails
+                    cache.put(key, sshDetails)
+                    logger.debug("SSH Detail Served from File Cache $sshDetails")
+                    sshDetails
+                }
+                catch (e: Exception) {
+                    logger.debug("Error loading Cache", e)
+                    logger.info("Error loading Cache from File $cacheFile")
+                    null
+                }
+            else -> null
+        }
+    }
+
+    fun cacheDetail(host: String, folder: String, sshDetails: SshDetails) {
+        val key = host + "-" + folder.replace("/".toRegex(), "-").replace("\\\\".toRegex(), "-")
+        val cacheFile = File(".cache/ssh-$key")
+
+        cache.put(key, sshDetails)
+        try {
+            xStream.toXML(sshDetails, FileOutputStream(cacheFile))
+        } catch (e: FileNotFoundException) {
+            logger.error("Saving cache for $cacheFile", e)
+        }
+    }
+
+}
--- src/main/java/org/neo/gomina/plugins/ssh/SshPlugin.kt
+++ src/main/java/org/neo/gomina/plugins/ssh/SshPlugin.kt
@@ -13,15 +13,34 @@ class SshPlugin : InstancesExt {
     @Inject private lateinit var sshConnector: SshOnDemandConnector
     @Inject private lateinit var inventory: Inventory
 
+    private val sshCache = SshCache()
+
     override fun onGetInstances(envId: String, instances: Instances) {
+        inventory.getEnvironment(envId)?.let { env ->
+            env.services
+                .flatMap { it.instances }
+                .filter { !it.host.isNullOrBlank() }
+                .filter { !it.folder.isNullOrBlank() }
+                .forEach {
+                    val id = env.id + "-" + it.id
+                    sshCache.getDetail(it.host!!, it.folder!!) ?. let {
+                        instances.get(id)?.applySsh(it)
+                    }
+                }
+        }
+    }
+
+    override fun onReloadInstances(envId: String) {
         inventory.getEnvironment(envId)?.let { env ->
             val analysis = sshConnector.analyze(env)
             env.services
-                .flatMap { it.instances }
-                .forEach {
-                    val id = env.id + "-" + it.id
-                    instances.get(id)?.applySsh(analysis.getFor(it.host, it.folder))
-                }
+                    .flatMap { it.instances }
+                    .filter { !it.host.isNullOrBlank() }
+                    .filter { !it.folder.isNullOrBlank() }
+                    .forEach {
+                        val sshDetails = analysis.getFor(it.host, it.folder)
+                        sshCache.cacheDetail(it.host!!, it.folder!!, sshDetails)
+                    }
         }
     }
 }
