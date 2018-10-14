73075921049718d6f87624028872a37916a10c54
Amokrane Belloui <amokrane.belloui@gmail.com>
2018-10-14 13:48:46 +0200

    Add 'Flow' dependency, for type checking

--- web/components/common/version-utils.js
+++ web/components/common/version-utils.js
@@ -1,21 +1,22 @@
+// @flow
 
-function isSnapshot(version) {
+function isSnapshot(version: String) {
     return version ? version.includes("-SNAPSHOT") : false;
 }
 
-function compareVersionsRevisions (a, reva, b, revb) {
+function compareVersionsRevisions (a: ?string, reva: ?number, b: ?string, revb: ?number) {
     let versions = compareVersions(a || '', b || '');
     return versions != 0 ? versions : (reva || 0) - (revb || 0);
 }
 
-function compareVersions (a, b) {
+function compareVersions (a: ?string, b: ?string) {
     a = a || '';
     b = b || '';
     var res = compareVersions2(a.replace("-SNAPSHOT", ""), b.replace("-SNAPSHOT", ""));
     return res != 0 ? res : (a.includes("-SNAPSHOT") ? 0 : 1) - (b.includes("-SNAPSHOT") ? 0 : 1);
 }
 
-function compareVersions2 (a, b) {
+function compareVersions2 (a: string, b: string) {
     var i, diff;
     var regExStrip0 = /(\.0+)+$/;
     var segmentsA = a.replace(regExStrip0, '').split('.');
